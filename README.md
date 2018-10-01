3285788fb789b0fa7d5cf5521ee5ad6d8b5fb610
Tanuj Aswani <tanuj.aswani@gmail.com>
2018-09-30 21:30:14 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Node-Email-App-Tanuj-Aswani
+
+Create an authentication.js file with an account object containing your email and password with user and pass keys. Npm install then npm start should run the program.
---
+++ app.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+const sendList = ["tanuj.aswani@gmail.com", "tanuj.aswani618@gmail.com"];
+const { account } = require("./authentication.js");
+
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  auth: {
+    user: account.user,
+    pass: account.pass
+  }
+});
+
+const mailOptions = {
+  from: "tanuj.aswani@hotmail.com",
+  to: sendList,
+  subject: "Testing 123",
+  text:
+    "This is a blast email with a generic message to send out to two different people.",
+  html: '<img src="cid:uniqueImage"/>',
+  attachments: [
+    {
+      filename: "scenery.jpeg",
+      path: "./scenery.jpeg",
+      cid: "uniqueID"
+    }
+  ]
+};
+
+transporter.sendMail(mailOptions, function(error) {
+  return error
+    ? console.log(error)
+    : console.log("Messages sent successfully!");
+});
